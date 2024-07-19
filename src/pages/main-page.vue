c7f6f68a754e81d1639bcc5e38305b16c8e7c654
abduazizaminov <abduazizaminov8@gmail.com>
2024-07-19 10:24:42 +0500

    init

---
+++ src/pages/main-page.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">
+    <h1>Main Page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+
+.main {
+  color: var(--primary-color);
+}
+</style>
